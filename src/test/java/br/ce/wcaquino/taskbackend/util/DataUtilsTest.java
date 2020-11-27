d8fdd3c49de0b9260585dba35352dea054db0ad8
Arquimedes Junior <arquimedeslqjunior@gmail.com>
2020-11-27 04:10:21 -0300

    criação de classes de testes unitários

---
+++ src/test/java/br/ce/wcaquino/taskbackend/util/DataUtilsTest.java
@@ -0,0 +1,28 @@
+package br.ce.wcaquino.taskbackend.util;
+
+import br.ce.wcaquino.taskbackend.utils.DateUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+public class DataUtilsTest {
+
+    @Test
+    public void deveRetornarTrue(){
+        LocalDate date = LocalDate.of(2030,01,01);
+        Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+    }
+
+    @Test
+    public void deveRetornarFalse(){
+        LocalDate date = LocalDate.of(2010,01,01);
+        Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
+    }
+
+    @Test
+    public void deveRetornarTrueParaDataAtual(){
+        LocalDate date = LocalDate.now();
+        Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+    }
+}
